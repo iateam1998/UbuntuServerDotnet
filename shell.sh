77547c89782becd817c05a5fd017bb6072e27f77
iateam1998 <47776129+iateam1998@users.noreply.github.com>
2019-07-17 21:55:09 +0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -6,15 +6,15 @@ sudo dpkg -i packages-microsoft-prod.deb
 
 #Install the .NET SDK
 sudo add-apt-repository universe
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.2=2.2.203-1
+sudo apt-get install dotnet-sdk-2.2=2.2.203-1 -y
 
 #Install nmap
 sudo apt-get update
-sudo apt-get install nmap
+sudo apt-get install nmap -y
 
 #Install sqlmap
-sudo apt-get install python2.7
+sudo apt-get install python2.7 -y
 sudo apt-get update
-sudo apt-get install sqlmap
+sudo apt-get install sqlmap -y
